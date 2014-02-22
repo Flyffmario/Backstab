a893faab4f758dfd3191282768538bca89206eca
Flyffmario <flyffmario@laposte.net>
2014-02-22 19:20:46 +0100

    Added config and modifications for config

--- src/main/java/net/minekingdom/backstab/BackStabListener.java
+++ src/main/java/net/minekingdom/backstab/BackStabListener.java
@@ -1,7 +1,11 @@
 package net.minekingdom.backstab;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,6 +16,20 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 public class BackStabListener implements Listener {
+	
+	public Set<Material> items;
+	
+	public BackStabListener(BackStab plugin) {
+		this.items = new HashSet<Material>();
+		ConfigurationSection section = plugin.getConfig().getConfigurationSection("items");
+		for (String key : section.getKeys(false)) {
+			Material item = Material.matchMaterial(key);
+			if (item == null) {
+				
+			}
+			this.items.add(item);
+		}
+	}
 	
 	@EventHandler
 	public void onDamage(EntityDamageByEntityEvent event, EntityDamageEvent event2) {
@@ -32,29 +50,26 @@ public class BackStabListener implements Listener {
 		
 		if (damagedV.angle(damagerV) <= 225 && damagedV.angle(damagerV) >= 135) {
 			
-			ItemStack item = ((Player) damager).getItemInHand();
+			Player damagedPlayer = ((Player) damaged);
+			Player damagerPlayer = ((Player) damager);
+			ItemStack item = damagerPlayer.getItemInHand();
 			
-			if (item.getType() == Material.DIAMOND_SWORD) {
+			if ((damagedPlayer.hasPermission("minekingdom.backStabbed") || (damagerPlayer.hasPermission("minekingdom.backStabber")))) {
+				
+			}
+			
+			if (items.contains(item.getType())) {
 				
 				if (event2.getEntity().equals(damaged)) {
 					
 					Double damage = event2.getDamage();
-					Player backstabber = ((Player) damager);
 					
 					event2.setDamage(damage*2);
-					backstabber.sendMessage(ChatColor.GREEN + "BackStab done !");
+					damagerPlayer.sendMessage(ChatColor.GREEN + "Backstab succeded !");
+					damagedPlayer.sendMessage(ChatColor.RED + "You've been Backstabbed !");
 					
 				}
-				
-
-				
-
-			}
-			
+			}	
 		}
-		
-		
-		
-		
 	}
 }
